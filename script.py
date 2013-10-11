e0be035de2960ebe601888dcb55534892c15b7d9
Bas van der Hoek <bas@hoek.com>
2013-10-11 14:03:34 +0200

    read script options from file

--- script.py
+++ script.py
@@ -1,2 +1,11 @@
-data =  eval(sys.argv[0])
-print data['node']['name']
+print "Processing %s" % sys.argv[0]
+dict_data = open(sys.argv[0]).read()
+dict = eval(dict_data)
+
+nodeName = dict['node']['name']
+node = AdminConfig.getid('/Node:%s/' % nodeName)
+
+serverName = dict['node']['server']['name']
+server = AdminConfig.create('Server', node, [['name', serverName]])
+
+print dict['node']['name']

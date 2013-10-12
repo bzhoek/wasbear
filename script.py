77350d65cca30b1f94bda1cdd9a5198adba39be6
Bas van der Hoek <bas@hoek.com>
2013-10-12 11:53:51 +0200

    create jdbc provider and datasource

--- script.py
+++ script.py
@@ -5,7 +5,31 @@ dict = eval(dict_data)
 nodeName = dict['node']['name']
 node = AdminConfig.getid('/Node:%s/' % nodeName)
 
-serverName = dict['node']['server']['name']
-server = AdminConfig.create('Server', node, [['name', serverName]])
+def createServer(param):
+  serverName = param['node']['server']['name']
+  server = AdminConfig.create('Server', node, [['name', serverName]])
+
+def createDataSource(param, providerName):
+  provider = AdminConfig.getid("/Node:%s/JDBCProvider/%s" % (nodeName, providerName))
+  dataSourceName = param['name']
+  print "Creating data source %s" % dataSourceName
+  dataSource = AdminConfig.getid("/Node:%s/JDBCProvider:%s/DataSource:%s" % (nodeName, providerName, dataSourceName ))
+  mapping = ['mapping',[['authDataAlias', param['j2c']], ['mappingConfigAlias', param['mapping']]]]
+  attrs = [['name', dataSourceName], ['jndiName', param['jndiName']], mapping]
+  dataSource = AdminConfig.create('DataSource', provider, attrs)
+
+def createJdbcProvider(param):
+  providerName = param['name']
+  print "Creating JDBC Provider %s" % providerName
+  provider = AdminConfig.getid("/Node:%s/JDBCProvider/%s" % (nodeName, providerName))
+  attrs = [['name', param['name']],
+    ['classpath', param['classpath']],
+    ['implementationClassName', param['implementationClassName']],
+    ['description', param['description']]]
+
+  provider = AdminConfig.create('JDBCProvider', node, attrs)
+  createDataSource(param['dataSource'], provider)
 
 print dict['node']['name']
+
+createJdbcProvider(dict['node']['jdbc'])

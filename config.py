7963c11633466fc1b10faa34581a07df3f92a90c
Bas van der Hoek <bas@hoek.com>
2013-10-12 11:55:28 +0200

    sample jdbc provider and data source

--- config.py
+++ config.py
@@ -16,3 +16,13 @@ rep = AdminConfig.getid('/Node:%s/Server:%s/ResourceEnvironmentProvider:%s' % (n
 # stringNameSpaceBinding
 AdminConfig.create('StringNameSpaceBinding', server, [['name', 'binding1'], ['nameInNameSpace', 'myBindings/myString'], ['stringToBind', "This is the String value that gets bound"]])
 binding = AdminConfig.getid('/Node:%s/Server:%s/StringNameSpaceBinding:%s' % (nodeName, serverName, "binding1"))
+
+# jdbcProvider
+providerName = 'DB2JDBCProvider'
+provider = AdminConfig.getid("%s/JDBCProvider/%s" % (nodeName, providerName))
+
+# jaas
+cellName = "precise64Node01Cell"
+security = AdminConfig.getid("/Cell:%s/Security:/" % cellName)
+authAlias = "precise64Node01/db2inst1"
+AdminConfig.create('JAASAuthData', security,  [['alias', authAlias], ['userId', "db2inst1"], ['password', "p@ssw0rd"], ['description', '']])

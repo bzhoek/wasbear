9ddeb8a33cf5fefea3ed263f85e1d60ae3df6c1d
Bas van der Hoek <bas@hoek.com>
2013-10-12 16:48:14 +0200

    create jaas credentials and data source connection properties

--- script.py
+++ script.py
@@ -3,9 +3,12 @@ dict_data = open(sys.argv[0]).read()
 dict = eval(dict_data)
 
 separator = java.lang.System.getProperty("line.separator")
-
 nodeName = dict['node']['name']
 node = AdminConfig.getid('/Node:%s/' % nodeName)
+
+
+def printTypeName(type, param):
+  print "Creating %s: %s" % (type, param['name'])
 
 
 def showAttribute(object, name):
@@ -13,23 +16,33 @@ def showAttribute(object, name):
 
 
 def createServer(param):
+  printTypeName('server', param)
   serverName = param['node']['server']['name']
   server = AdminConfig.create('Server', node, [['name', serverName]])
 
 
-def createDataSource(param, providerName):
-  provider = AdminConfig.getid("/Node:%s/JDBCProvider/%s" % (nodeName, providerName))
+def createDataSource(param, provider):
+  printTypeName('data source', param)
+  providerName = showAttribute(provider, 'name')
   dataSourceName = param['name']
-  print "Creating data source %s" % dataSourceName
   dataSource = AdminConfig.getid("/Node:%s/JDBCProvider:%s/DataSource:%s" % (nodeName, providerName, dataSourceName ))
   mapping = ['mapping', [['authDataAlias', param['j2c']], ['mappingConfigAlias', param['mapping']]]]
   attrs = [['name', dataSourceName], ['jndiName', param['jndiName']], mapping]
   dataSource = AdminConfig.create('DataSource', provider, attrs)
+  propertySet = AdminConfig.create('J2EEResourcePropertySet', dataSource, [])
+  AdminConfig.create('J2EEResourceProperty', propertySet, [['name', 'driverType'], ['type', 'java.lang.Integer'],
+    ['value', param['driverType']]])
+  AdminConfig.create('J2EEResourceProperty', propertySet, [['name', 'databaseName'], ['type', 'java.lang.String'],
+    ['value', param['databaseName']]])
+  AdminConfig.create('J2EEResourceProperty', propertySet, [['name', 'serverName'], ['type', 'java.lang.String'],
+    ['value', param['serverName']]])
+  AdminConfig.create('J2EEResourceProperty', propertySet, [['name', 'portNumber'], ['type', 'java.lang.Integer'],
+    ['value', param['portNumber']]])
 
 
 def createJdbcProvider(param):
+  printTypeName('JDBC provider', param)
   providerName = param['name']
-  print "Creating JDBC Provider %s" % providerName
   provider = AdminConfig.getid("/Node:%s/JDBCProvider/%s" % (nodeName, providerName))
   attrs = [['name', param['name']],
     ['classpath', param['classpath']],
@@ -41,6 +54,7 @@ def createJdbcProvider(param):
 
 
 def createVariable(param):
+  printTypeName('variable', param)
   variables = AdminConfig.list('VariableSubstitutionEntry', node).split(separator)
   for variable in variables:
     if param['name'] == showAttribute(variable, 'symbolicName'):
@@ -50,8 +64,19 @@ def createVariable(param):
   AdminConfig.create('VariableSubstitutionEntry', map, [['symbolicName', param['name']], ['value', param['value']]])
 
 
+def createCredential(param):
+  printTypeName('credentials', param)
+  security = AdminConfig.getid("/Cell:%sCell/Security:/" % nodeName)
+  authAlias = "%s/%s" % (nodeName, param['name'])
+  AdminConfig.create('JAASAuthData', security, [['alias', authAlias], ['userId', param['username']], ['password',
+    param['password']], ['description', '']])
+  print "NOTE: REQUIRES SERVER RESTART"
+
+
 print dict['node']['name']
 
-# createJdbcProvider(dict['node']['jdbc'])
-createVariable(dict['node']['variable'])
+# createCredential(dict['node']['j2c'])
+# createVariable(dict['node']['variable'])
+createJdbcProvider(dict['node']['jdbc'])
+
 AdminConfig.save()

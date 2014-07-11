497d66f57ddcbd0d45878496306c2955ae3f07a1
Bas van der Hoek <bvdhoek@zilverline.com>
2014-07-11 12:38:39 +0200

    make infile and outfile command-line parameters

--- config.py
+++ config.py
@@ -1,12 +1,17 @@
-try:
-    import json
-except ImportError:
-    import simplejson as json
+# Converts a JSON file to a serialized dictionary, that can be deserialized by an Jython 2.1
+import sys
 
-json_data = open('config.json')
+try:
+  import json
+except ImportError:
+  import simplejson as json
+
+infile = (len(sys.argv) > 1) and sys.argv[1] or "config.json"
+outfile = (len(sys.argv) > 2) and sys.argv[2] or "config.dict"
+
+json_data = open(infile)
 data = json.load(json_data)
-dict_data = open('config.dict', 'w')
+dict_data = open(outfile, 'w')
 dict_data.write(str(data))
 
-# {u'node': {u'name': u'precise64Node01', u'server': {u'name': u'server2'}}}
-# atad = eval("{u'node': {u'name': u'precise64Node01', u'server': {u'name': u'server2'}}}")
+print "Converted '%s' to '%s'" % (infile, outfile)

9767ec12003fbfb9b4b36b2a064f8ddd1fc77cf2
Bas van der Hoek <bvdhoek@zilverline.com>
2014-07-17 13:15:42 +0200

    make path to gson absolute

--- wsadmin.sh
+++ wsadmin.sh
@@ -1,1 +1,1 @@
-rlwrap wsadmin.sh -user wasadmin -password p@ssw0rd -lang jython -wsadmin_classpath ./gson-2.2.4.jar
+rlwrap wsadmin.sh -user wasadmin -password p@ssw0rd -lang jython -wsadmin_classpath /wasbear/gson-2.2.4.jar
